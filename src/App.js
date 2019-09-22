86caf343e801e88ef644566b3aef03fa89edb210
Tin Diep <tindieptrung@gmail.com>
2019-09-22 15:39:03 +0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I am studying Docker and Kubernetes on Udemy
         </p>
         <a
           className="App-link"
